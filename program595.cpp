ef9ddd819279ef7e11d76a7c59e353c1242f02d3
snehalshitkal <snehalshitkal23@gmail.com>
2025-08-18 23:03:19 +0530

    Add files via upload

---
+++ program595.cpp
@@ -0,0 +1,106 @@
+/////////////////////////////////////////////////////////////////////////////////
+
+//       Program   :   Insertion Sort Algorithm
+//       Date      :   17/8/2025
+//       Author    :   snehal rohit shitkal
+
+/////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+class ArrayX                                        
+{
+    public:
+        int *Arr;
+        int iSize;
+        bool Sorted;
+
+        ArrayX(int no)
+        {
+            iSize = no;
+            Arr = new int[iSize];
+            Sorted = true;
+        }
+
+        ~ArrayX()
+        {
+            delete []Arr;
+        }
+
+        void Accept()
+        {
+            cout<<"Enter "<<iSize<<" elements : \n";
+            
+            int i = 0;                       
+
+            for(i = 0; i < iSize;i++)
+            {
+                cout<<"Enter the element no : "<<i+1<<"\n";
+                cin>>Arr[i];
+
+                if((i > 0) && (Sorted == true))                 
+                {
+                    if(Arr[i] < Arr[i-1])
+                    {
+                        Sorted = false;
+                    }
+                }
+            }
+        }
+
+        void Display()
+        {
+            cout<<"Elements of the array are : \n";
+            
+            int i = 0;
+
+            for(i = 0; i < iSize; i++)
+            {
+                cout<<Arr[i]<<"\t";
+            }
+            cout<<"\n";
+        }
+ 
+        void InsertionSort()
+        {
+            int i = 0, j = 0, selected = 0;
+
+            for(i = 1; i < iSize; i++)
+            {
+                for(j = i - 1, selected = Arr[i]; (j >= 0) && (Arr[j] > selected); j--)
+                {
+                    Arr[j + 1] = Arr[j];
+                }
+                Arr[j + 1] = selected;
+            }
+        }
+
+};
+
+int main()
+{
+    int iLength = 0;
+    int iValue = 0;
+
+    bool bRet = false;
+
+    cout<<"Enter the size of array : \n";
+    cin>>iLength;
+
+    ArrayX *aobj = new ArrayX(iLength);
+
+    aobj->Accept();
+
+    aobj->Display();
+
+    aobj->InsertionSort();
+
+    cout<<"Data after sorting : \n";
+
+    aobj->Display();
+   
+    delete aobj;
+    
+    return 0;
+}
